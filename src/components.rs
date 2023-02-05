8c21a1fe8951aba5ab925fde563877f945a71c61
Daniel Boros <dancixx@gmail.com>
2023-02-05 02:38:57 +0100

    add snake move

--- src/components.rs
+++ src/components.rs
@@ -1,22 +1,45 @@
 use bevy::prelude::Component;
 
-#[derive(Component, Debug, PartialEq, Eq)]
-pub struct SnakePointID(pub usize);
+#[derive(Component, Clone, Debug)]
+pub struct SnakePoints(pub Vec<Point>);
 
-#[derive(Component, Debug)]
-pub struct SnakePoint(pub f32, pub f32);
+impl SnakePoints {
+    pub fn new() -> Self {
+        Self(vec![{ Point { x: 0_f32, y: 0_f32 } }])
+    }
+
+    pub fn head(&self) -> Point {
+        self.0[0]
+    }
+
+    pub fn tail(&self) -> Point {
+        self.0[self.0.len() - 1]
+    }
+
+    pub fn add_head(&mut self, point: Point) {
+        self.0.insert(0, point);
+    }
+
+    pub fn remove_tail(&mut self) {
+        self.0.pop();
+    }
+
+    pub fn push(&mut self, point: Point) {
+        self.0.push(point);
+    }
+}
 
 #[derive(Component)]
 pub struct LastDirection(pub Direction);
 
 // point number
-#[derive(Component)]
+#[derive(Component, Clone, Copy, Debug)]
 pub struct Point {
     pub x: f32,
     pub y: f32,
 }
 
-#[derive(Component, Debug)]
+#[derive(Component, Debug, PartialEq, Eq)]
 pub enum Direction {
     Up,
     Down,
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ pub mod resources;
 use bevy::core_pipeline::clear_color::ClearColorConfig;
 use bevy::prelude::Color;
 use bevy::prelude::*;
-use components::SnakePointID;
+use rand::Rng;
+
+use crate::components::Point;
 
 fn main() {
     App::new()
@@ -22,8 +24,10 @@ fn main() {
             TimerMode::Repeating,
         )))
         .add_startup_system(setup)
-        .add_system(snake_head_movement_system)
-        .add_system(build_snake_body)
+        .add_system(controller)
+        .add_system(gen_random_food)
+        .add_system(move_snake)
+        .add_system(render_snake)
         .run();
 }
 
@@ -40,7 +44,7 @@ fn setup(mut commands: Commands) {
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(800_f32, 600_f32)),
+            custom_size: Some(Vec2::new(816_f32, 624_f32)),
             ..Default::default()
         },
         ..Default::default()
@@ -56,16 +60,16 @@ fn setup(mut commands: Commands) {
             },
             ..Default::default()
         },
-        components::SnakePointID(0_usize),
-        components::SnakePoint(0_f32, 0_f32),
+        components::SnakePoints::new(),
     ));
 
     // set default direction
     commands.spawn(components::LastDirection(components::Direction::Left));
 
     // add first random point
-    let random_x = f32::ceil((rand::random::<f32>() * 800_f32 - 416_f32) / 16_f32) * 16_f32;
-    let random_y = f32::ceil((rand::random::<f32>() * 800_f32 - 316_f32) / 16_f32) * 16_f32;
+    let mut rng = rand::thread_rng();
+    let random_x = f32::ceil(rng.gen_range(-400_f32..400_f32) / 16_f32) * 16_f32;
+    let random_y = f32::ceil(rng.gen_range(-300_f32..300_f32) / 16_f32) * 16_f32;
 
     // add the point
     commands.spawn((
@@ -91,117 +95,118 @@ fn setup(mut commands: Commands) {
     ));
 }
 
-fn snake_head_movement_system(
-    keyboard_input: Res<Input<KeyCode>>,
+fn move_snake(
     time: Res<Time>,
     mut step_timer: ResMut<resources::StepTimer>,
     mut direction_query: Query<&mut components::LastDirection>,
-    mut snake_head_query: Query<(
-        &mut components::SnakePoint,
-        &components::SnakePointID,
-        &mut Transform,
-    )>,
+    mut snake_query: Query<&mut components::SnakePoints>,
+    point_query: Query<&components::Point>,
 ) {
     // get the snake head and its transform
-    let (mut snake, id, mut transform) = snake_head_query.single_mut();
+    let mut snake = snake_query.single_mut();
+    let snake_head = snake.0[0];
+
     // get the last direction
-    let mut direction = direction_query.single_mut();
+    let direction = direction_query.single_mut();
+    // get food point
+    let point = point_query.single();
 
-    // check if the snake head is out of the screen and move it to the other side
-    if snake.0 >= 416_f32 {
-        transform.translation.x = -400_f32;
-        snake.0 = -400_f32;
-    }
+    let mut dx = 16_f32;
+    let mut dy = 0_f32;
 
-    if snake.0 <= -416_f32 {
-        transform.translation.x = 400_f32;
-        snake.0 = 400_f32;
-    }
-
-    if snake.1 >= 304_f32 {
-        transform.translation.y = -300_f32;
-        snake.1 = -300_f32;
-    }
-
-    if snake.1 <= -304_f32 {
-        transform.translation.y = 300_f32;
-        snake.1 = 300_f32;
-    }
-
-    // check if the user pressed a key and change the direction
-    if keyboard_input.pressed(KeyCode::Up) {
-        direction.0 = components::Direction::Up;
-    } else if keyboard_input.pressed(KeyCode::Down) {
-        direction.0 = components::Direction::Down;
-    } else if keyboard_input.pressed(KeyCode::Left) {
-        direction.0 = components::Direction::Left;
-    } else if keyboard_input.pressed(KeyCode::Right) {
-        direction.0 = components::Direction::Right;
-    }
-
-    // move the snake head
-    if step_timer.0.tick(time.delta()).just_finished() {
-        match direction.0 {
-            components::Direction::Up => transform.translation.y += 16_f32,
-            components::Direction::Down => transform.translation.y -= 16_f32,
-            components::Direction::Left => transform.translation.x -= 16_f32,
-            components::Direction::Right => transform.translation.x += 16_f32,
+    match direction.0 {
+        components::Direction::Up => {
+            dx = 0_f32;
+            dy = 16_f32;
+        }
+        components::Direction::Down => {
+            dx = 0_f32;
+            dy = -16_f32;
+        }
+        components::Direction::Left => {
+            dx = -16_f32;
+            dy = 0_f32;
+        }
+        components::Direction::Right => {
+            dx = 16_f32;
+            dy = 0_f32;
         }
     }
 
-    // update the snake head position
-    snake.0 = transform.translation.x;
-    snake.1 = transform.translation.y;
+    let head = components::Point {
+        x: snake.0[0].x + dx,
+        y: snake.0[0].y + dy,
+    };
+
+    if step_timer.0.tick(time.delta()).just_finished() {
+        snake.add_head(head);
+
+        if !(snake_head.x == point.x && snake_head.y == point.y) {
+            snake.remove_tail();
+        } else {
+            snake.push(Point {
+                x: snake_head.x,
+                y: snake_head.y,
+            })
+        }
+    }
 }
 
-fn build_snake_body(
-    mut commands: Commands,
+fn render_snake(mut commands: Commands, snake_query: Query<&components::SnakePoints>) {
+    let snake = snake_query.single();
+
+    for snake_point in snake.0.iter() {
+        commands.spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(16_f32, 16_f32)),
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(snake_point.x, snake_point.y, 0_f32),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
+    }
+}
+
+fn controller(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut direction: Query<&mut components::LastDirection>,
+) {
+    let mut direction = direction.single_mut();
+
+    if keyboard_input.pressed(KeyCode::Up) && direction.0 != components::Direction::Down {
+        direction.0 = components::Direction::Up;
+    } else if keyboard_input.pressed(KeyCode::Down) && direction.0 != components::Direction::Up {
+        direction.0 = components::Direction::Down;
+    } else if keyboard_input.pressed(KeyCode::Left) && direction.0 != components::Direction::Right {
+        direction.0 = components::Direction::Left;
+    } else if keyboard_input.pressed(KeyCode::Right) && direction.0 != components::Direction::Left {
+        direction.0 = components::Direction::Right;
+    }
+}
+
+fn gen_random_food(
     mut point_query: Query<(&mut components::Point, &mut Transform)>,
-    snake_query: Query<(&mut components::SnakePoint, &SnakePointID)>,
+    mut snake_query: Query<&mut components::SnakePoints>,
 ) {
     let (mut point, mut transform) = point_query.single_mut();
+    let snake = snake_query.single_mut();
+    let snake_head = snake.0[0];
 
-    for (snake_point, id) in snake_query.iter() {
-        if id == &components::SnakePointID(0_usize)
-            && snake_point.0 == point.x
-            && snake_point.1 == point.y
-        {
-            // get the last snake point
-            let last_snake_point = snake_query.iter().last().expect("No snake points found");
-            commands.spawn((
-                SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::WHITE,
-                        custom_size: Some(Vec2::new(16_f32, 16_f32)),
-                        ..Default::default()
-                    },
-                    transform: Transform {
-                        translation: Vec3::new(
-                            (last_snake_point.0).0 + 16_f32,
-                            (last_snake_point.0).1 + 16_f32,
-                            0_f32,
-                        ),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-                components::SnakePointID(&id.0 + 1),
-                components::SnakePoint(
-                    (last_snake_point.0).0 + 16_f32,
-                    (last_snake_point.0).1 + 16_f32,
-                ),
-            ));
+    if snake_head.x == point.x && snake_head.y == point.y {
+        let mut rng = rand::thread_rng();
+        let random_x = f32::ceil(rng.gen_range(-400_f32..400_f32) / 16_f32) * 16_f32;
+        let random_y = f32::ceil(rng.gen_range(-300_f32..300_f32) / 16_f32) * 16_f32;
 
-            let random_x = f32::ceil((rand::random::<f32>() * 800_f32 - 400_f32) / 16_f32) * 16_f32;
-            let random_y = f32::ceil((rand::random::<f32>() * 800_f32 - 400_f32) / 16_f32) * 16_f32;
+        // update the point position
+        transform.translation.x = random_x;
+        transform.translation.y = random_y;
 
-            // update the point position
-            transform.translation.x = random_x;
-            transform.translation.y = random_y;
-
-            // update the point details
-            point.x = transform.translation.x;
-            point.y = transform.translation.y;
-        }
+        // update the point details
+        point.x = transform.translation.x;
+        point.y = transform.translation.y;
     }
 }
